654361aafd1e89f05ef820fe7ae2b52b0af0b7cf
Synesthesya <alex.giaco91@libero.it>
2015-12-11 17:15:46 +0100

    tolto sleep da .bat

--- launcher.bat
+++ launcher.bat
@@ -2,6 +2,4 @@ start java -Djava.rmi.server.logCalls=true -Djava.rmi.server.codebase=http://127
 
 start java -Djava.rmi.server.logCalls=true -Djava.rmi.server.codebase=http://127.0.0.1:8081/ -Djava.security.policy=bin/launcher/client.policy -cp "bin" launcher.Start
 
-sleep(1)
-
 java -Djava.rmi.server.logCalls=true -Djava.rmi.server.codebase=http://127.0.0.1:8081/ -Djava.security.policy=bin/launcher/client.policy -cp "bin" launcher.Start
