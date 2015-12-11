815d5e3f38b695187c00b25a74e2caccdd201c50
Synesthesya <alex.giaco91@libero.it>
2015-12-11 16:15:36 +0100

    Aggiunta condizione while prima dell'iscrizione al server
    2 ore di lavoro! 2!!

--- Client/launcher/Start.java
+++ Client/launcher/Start.java
@@ -14,6 +14,8 @@ import core.Player;
  *
  */
 public class Start {
+	
+	public static final int MAX_TRIED = 5;
 
 	/**
 	 * main
@@ -29,28 +31,29 @@ public class Start {
 		
 		ServerI s;
 		Player p;
-		try
-		{
-		  s= (ServerI)Naming.lookup("rmi://127.0.0.1:1677/server");
-		  if(!s.registraPlayer())
-		  {
-			  System.out.println("Numero massimo di giocatori raggiunto");
-			  System.exit(1);
-		  }
-		  Integer ID=s.getID();
-		  p=new Player(ID);
-		  //creazione stub
-	      Naming.bind("rmi://127.0.0.1:1677/player"+ ID.toString(), p);
-	      Controller c=new MyShipController(p,s);
-	      p.setController(c);
-	      Frame f=new Frame(ID,c, null);
-	      c.setGrids(f);
-	      //comunica lo stub al server
-	      s.caricaPlayer();
-		}
-		catch(Exception e)
-		{
-		  System.err.println(e.getMessage());
+		int tried=0;
+		
+		
+		while (tried<MAX_TRIED) {
+			try {
+				s = (ServerI) Naming.lookup("rmi://127.0.0.1:1677/server");
+				if (!s.registraPlayer()) {
+					System.out.println("Numero massimo di giocatori raggiunto");
+					System.exit(1);
+				}
+				Integer ID = s.getID();
+				p = new Player(ID);
+				//creazione stub
+				Naming.bind("rmi://127.0.0.1:1677/player" + ID.toString(), p);
+				Controller c = new MyShipController(p, s);
+				p.setController(c);
+				Frame f = new Frame(ID, c, null);
+				c.setGrids(f);
+				//comunica lo stub al server
+				s.caricaPlayer();
+			} catch (Exception e) {
+				System.err.println(e);
+			} 
 		}
 		
 		/*
