17e6cd40cf13d39dfa59cdde7959f23af5a7ab50
Zorgio <giorgiomaria.amatteis@virgilio.it>
2015-12-14 20:08:40 +0100

    MODIFICATO IL MENU
    Ora funziona.
    Trovo insopportabile la vostra mancanza di fede.

--- Client/graphic/menu/GraphicStart.java
+++ Client/graphic/menu/GraphicStart.java
@@ -19,35 +19,11 @@ public class GraphicStart {
 	 * @param argv
 	 */
 	 public static void main(String... argv) {
-		    JFrame frame = new JFrame("Demo Grafica");
-		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		    /*
-		    Background back = new Background(".//bin//vader.png");
-		    frame.add(back);
-		    frame.setSize(1200, 800);
-		    frame.setVisible(true);
-		    back.setLayout(null);
-		    
-		    JButton button1 = new JButton("MULTIPLAYER");
-		    JButton button2 = new JButton("SINGLEPLAYER");
-		    JButton button3 = new JButton("OPZIONI");
-		    JButton button4 = new JButton("ESCILE");
-		    
-		    back.add(button1);
-		    back.add(button2);
-		    back.add(button3);
-		    back.add(button4);
-		  
-		    button1.setBounds(50, 50, 300, 100);
-		    button2.setBounds(50, 200, 300, 100);
-		    button3.setBounds(50, 350, 300, 100);
-		    button4.setBounds(50, 500, 300, 100);
-		    */
-		    
-		    MainMenu mm=new MainMenu();
-		    frame.add(mm);
-		    frame.setSize(1200, 800);
-		    frame.setVisible(true);
-		    
+		 JFrame frame = new JFrame("Demo Background Image");
+		 MainMenu menu = new MainMenu();
+		 frame.add(menu);
+		 frame.setSize(1200, 800);
+		 frame.setResizable(false);
+		 frame.setVisible(true);
 		  }
 }
--- Client/graphic/menu/MainMenu.java
+++ Client/graphic/menu/MainMenu.java
@@ -2,6 +2,12 @@ package graphic.menu;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+
 
 /**
  * Classe che rappresenta graficamente il menù principale
@@ -11,38 +17,48 @@ import javax.swing.JPanel;
  *
  */
 public class MainMenu extends JPanel {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * costruttore standard
-	 */
-	public MainMenu() {
-		
-		Background back = new Background(".//bin//vader.png");
-		this.add(back);
-		this.setSize(1200, 800);
-	    back.setLayout(null);
+	  private Image img;
+
+	  public MainMenu() {
+		String path = ".//bin//vader.png";
+	    img = Toolkit.getDefaultToolkit().createImage(path);
+	    loadImage(img);
+	    setLayout(null);
 	    
 	    JButton button1 = new JButton("MULTIPLAYER");
 	    JButton button2 = new JButton("SINGLEPLAYER");
 	    JButton button3 = new JButton("OPZIONI");
 	    JButton button4 = new JButton("ESCILE");
 	    
-	    back.add(button1);
-	    back.add(button2);
-	    back.add(button3);
-	    back.add(button4);
+	    add(button1);
+	    add(button2);
+	    add(button3);
+	    add(button4);
 	    
 	    button1.setBounds(50, 50, 300, 100);
+	    ImageIcon iconB1 = new ImageIcon("/home/giorgio/Immagini/squirtle.png");
+	    button1.setIcon(iconB1);
+	    
 	    button2.setBounds(50, 200, 300, 100);
 	    button3.setBounds(50, 350, 300, 100);
 	    button4.setBounds(50, 500, 300, 100);
-	    
-	    
-	    
+	  }
+
+	  private void loadImage(Image img) {
+	    try {
+	      MediaTracker track = new MediaTracker(this);
+	      track.addImage(img, 0);
+	      track.waitForID(0);
+	    } catch (InterruptedException e) {
+	      e.printStackTrace();
+	    }
+	  }
+
+	  protected void paintComponent(Graphics g) {
+	    setOpaque(false);
+	    g.drawImage(img, 0, 0, null);
+	    super.paintComponent(g);
+	  }
+
 	}
-}
